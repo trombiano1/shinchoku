98f951c11b280ebd540468434d7ac961cd4727c2
trombiano1 <trombiano1@gmail.com>
2023-05-25 12:10:30 +0900

    refactor

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const data = fs.readFileSync("codes.csv")
 const records = parse(data, { delimiter: ",", columns: true });
 const codes = records.map(item => item.code.toLowerCase());
 
+app.get("/index?", (req, res, next) => {
+    res.render('index', { url: req.url, records: records });
+});
+
 app.get("/code/:var?", (req, res, next) => {
     var code_str;
     if (req.params.var === undefined) {
@@ -20,6 +24,14 @@ app.get("/code/:var?", (req, res, next) => {
     } else {
         code_str = "404";
     }
+    displayError(code_str, req, res);
+});
+
+app.use((req, res, next) => {
+    displayError(404, req, res);
+});
+
+function displayError(code_str, req, res) {
     const code = parseInt(code_str);
     const record = records.find(item => item.code == code_str);
     const status = record.status;
@@ -33,40 +45,13 @@ app.get("/code/:var?", (req, res, next) => {
     }
     
     if (req.accepts('json')) {
-        res.json({ error: status });
-        return;
-    }
-    
-    res.type('txt').send(status);
-    res.writeHead(code, {'Content-Type': 'text/plain'});
-});
-
-app.get("/index?", (req, res, next) => {
-    // respond with html page
-    res.render('index', { url: req.url, records: records });
-});
-
-app.use((req, res, next) => {
-    const code = 404;
-    const record = records.find(item => item.code == "404");
-    const status = record.status;
-    const message = record.message;
-
-    res.status(code);
-    
-    if (req.accepts('html')) {
-        res.render('error', { url: req.url, code: "404", codeMessage: `${code} ${status}`, message: message });
-        return
-    }
-    
-    if (req.accepts('json')) {
         res.json({ error: status});
         return;
     }
     
-    res.type('txt').send('Processing');
-    res.writeHead(code, {'Content-Type': 'text/plain'});
-});
+    res.type('txt').send(status);
+    res.writeHead(code, {'Content-Type': 'text/plain'}); 
+}
 
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => console.log("Server is running ..."));
